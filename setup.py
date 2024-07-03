418df16101cd4581f69224985649021ca797d580
Thomas (Tom) Winckelman <thomas.k.winckelman@gmail.com>
2024-07-03 12:23:02 -0600

    Added a minimal setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+### ~~~
+## ~~~ From https://github.com/maet3608/minimal-setup-py
+### ~~~
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'quality_of_life',
+    version = '1.0.0',
+    url = 'https://github.com/ThomasLastName/quality_of_life',
+    author = 'Thomas Winckelman',
+    author_email = 'winckelman@tamu.edu',
+    description = 'Helper routines',
+    packages = find_packages(),    
+    install_requires = [],
+)
