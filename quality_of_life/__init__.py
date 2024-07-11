038e8fb39623a7bda3663c71b8c47613daf95b17
Thomas (Tom) Winckelman <thomas.k.winckelman@gmail.com>
2024-07-10 19:44:54 -0600

    Corrected the package name in __init__.py possibly responsible for crash

--- quality_of_life/__init__.py
+++ quality_of_life/__init__.py
@@ -1,4 +1,4 @@
 #
 # ~~~ Automatically correspond with the version from setup.py (chat-gpt showed me this one)
 from pkg_resources import get_distribution, DistributionNotFound
-__version__ = get_distribution('package_name').version
+__version__ = get_distribution('quality_of_life').version
